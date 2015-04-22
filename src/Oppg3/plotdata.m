88e30e553917c4bf0d4a09e225c8903da7b7c2c5
unknown <chrimath@ITK-D446.win.ntnu.no>
2015-04-22 12:12:00 +0200

    Last lab commit

--- src/Oppg3/plotdata.m
+++ src/Oppg3/plotdata.m
@@ -4,28 +4,19 @@ load closedLoop
 
 wait = 10;
 
-dt = 0.001;
-p_c = data(4,floor(wait/dt):end);
-p = data(6,floor(wait/dt):end);
-travel_c = data(2,floor(wait/dt):end) - pi;
-travel = data(7,floor(wait/dt):end) - data(7,floor(wait/dt)) - pi;
-time = (0:length(p_c)-1)*dt;
+h = data(1,2)-data(1,1);
+travel = data(2,floor(wait/h):end) - data(2,floor(wait/h)) - pi;
+pitch = data(3,floor(wait/h):end);
+time = (0:length(pitch)-1)*h;
+time_ = (0:N)*dt;
 
-figure
 hold on
-plot(time, p, 'b');
+plot(time_, pitch_opt, 'O:b');
+plot(time_, travel_opt, 'O:g');
+plot(time, pitch, 'b');
 plot(time, travel, 'g');
-plot(time, p_c, ':b');
-plot(time, travel_c, ':g');
-plot([0 time(end)],[0 0], ':k');
-xlim([0 16]);
 xlabel('Time [s]'); ylabel('Angle [rad]');
-legend('p', '\lambda','p_c','\lambda_c', '\lambda_f', 'Location', 'SouthEast');
-hold off
+legend('p^*', '\lambda^*', 'p', '\lambda', 'Location', 'SouthEast');
+xlim([0 12.8]);
 
-figure
-h = 0.25;
-plot((0:length(heli_input)-wait/h)*h, heli_input(wait/h:end,2), 'r', time, p_c, ':r');
-xlim([0 16]);
-xlabel('Time [s]'); ylabel('Angle [rad]');
-legend('u^*', 'u', 'Location', 'SouthEast'); 
+hold off
