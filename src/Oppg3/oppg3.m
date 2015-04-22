ee963526f1dcf37de87d991a216a9b7ab041a865
c-math <christer@fastmail.net>
2015-04-22 12:22:37 +0200

    Removed unnecessary redefinition

--- src/Oppg3/oppg3.m
+++ src/Oppg3/oppg3.m
@@ -4,8 +4,6 @@ Q = diag([4,2,0,0]);
 R = 0.1;
 
 x_opt = x + repmat(xf, 1, length(x)); % Shift travel ref by pi
-travel_opt = [-xf(1), x(1,:)];
-pitch_opt = [-xf(3), x(3,:)];
 
 padding_time = 10;
 padded_x_opt = [zeros(4,floor(padding_time/dt)) , x_opt];
