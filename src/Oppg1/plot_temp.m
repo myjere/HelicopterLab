9e49dde690a00aabacc9933eb66497308378a6cc
Eirik <eirikwlegernes@hotmail.com>
2015-04-28 13:03:56 +0200

    elevation model comparison adde

--- src/Oppg1/plot_temp.m
+++ src/Oppg1/plot_temp.m
@@ -27,9 +27,12 @@ plot(time,bad,'--k', time, good, 'k')
 xlabel('Time [s]'); ylabel('Elevation angle [rad]');
 legend('Original', 'Improved', 'Location', 'NorthWest');
 
+
+
+%% PITCH %%
 figure
 time = 0.001:0.001:5;
-%% Good model
+% Good model
 Ac = [0    1    0    0;
       0    -0.03 -0.39   0;
       0    0    0    1;
@@ -39,7 +42,7 @@ Bc = [ 0 0 0 6.74]';
 sys_good = ss(Ac,Bc,eye(4),[]);
 step_good = step(sys_good,time)*pi*40/180;
 
-%% Bad model
+% Bad model
 
 Acb = [0    1    0    0;
       0    0 -K_2   0;
@@ -50,10 +53,27 @@ Bcb = [ 0 0 0 K_1*K_pp]';
 sys_bad = ss(Acb,Bcb,eye(4),[]);
 step_bad = step(sys_bad,time)*pi*40/180;
 
-%% Measured response
+% Measured response
 load pitchStep40deg
 measured = pitchStep40deg.signals.values(1:5000)-pitchStep40deg.signals.values(1);
 
 plot(time,step_bad(:,3),'-.b',time,step_good(:,3),'--b',time,measured,'k')
+xlabel('Time [s]'); ylabel('Angle [rad]');
+legend('Derived', 'Estimated', 'Measured', 'Location', 'SouthEast');
+
+
+%% ELEVATION %%
+figure
+
+sys_bad = tf([K_3*K_ep],[1 K_3*K_ed K_3*K_ep]);
+step_bad = step(sys_bad,time)*pi*30/180;
+
+sys_good = tf([3.125],[1 2.435 3.028]);
+step_good = step(sys_good,time)*pi*30/180;
+
+load elevStep30deg
+measured = elevStep30deg.signals.values(1:5000) + 16.8*pi/180;
+
+plot(time,step_bad,'-.k',time,step_good,'--k',time,measured,'k')
 xlabel('Time [s]'); ylabel('Angle [rad]');
 legend('Derived', 'Estimated', 'Measured', 'Location', 'SouthEast');
